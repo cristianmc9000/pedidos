b3a84362bdbc79311cea4f6ed60abe7a91dcac36
cristianmc9000 <cristian.preof1@gmail.com>
2022-03-23 09:55:39 -0400

    hola mundo

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ES">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<center><h3>HOLA MUNDO</h3></center>
+</body>
+</html>
