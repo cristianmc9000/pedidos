00d63468db078816d9f484477f60bee1981ad26b
cristianmc9000 <cristian.preof1@gmail.com>
2022-03-30 17:30:51 -0400

    cambios menores

--- templates/catalogos/pagina.php
+++ templates/catalogos/pagina.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 	<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!-- Compiled and minified JavaScript -->
@@ -17,7 +17,14 @@
 	
 <h1>HOLA MUNDO</h1>
 
+
+<button class=""></button>
 <a href="recursos/salir.php">CERRAR SESIÓN</a>
 
 </body>
 </html>
+
+<script>
+	
+	
+</script>
