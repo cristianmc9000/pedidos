3b862b140df8331936f4641d9eab9f0acd047d5d
cristianmc9000 <cristian.preof1@gmail.com>
2022-04-05 16:34:21 -0400

    conexion

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 
 // $conexion = new mysqli('localhost','id16777589_root','papapapa1212AC!','id16777589_base_arbell'); 000webhost
-// $conexion = new mysqli('localhost','u604223767_root','papapapa1212AC','u604223767_base_arbell'); //codemid.net
-$conexion = new mysqli('localhost','root','','base_arbell');
+$conexion = new mysqli('localhost','u604223767_root','papapapa1212AC','u604223767_base_arbell'); //codemid.net
+// $conexion = new mysqli('localhost','root','','base_arbell');
 
 //$conexion = new mysqli('br6vrwqejcpdav29tluj-mysql.services.clever-cloud.com','usz8khcb6vchmslt','1DjIm6ncGfmq6N81hRtt','br6vrwqejcpdav29tluj'); //clever cloud
 
