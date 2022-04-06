e4eb081a46a36bf01428dc3dffa52189928728d4
cristianmc9000 <cristian.preof1@gmail.com>
2022-04-06 17:37:45 -0400

    pdf view

--- templates/catalogos/pagina.php
+++ templates/catalogos/pagina.php
@@ -11,12 +11,20 @@
 	<link rel="stylesheet" href="css/index.css">
 	<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+		integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous"
+		referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="css/viewpdf.css">
+
     <script src="js/jquery-3.0.0.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="js/jquery.nice-number.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.9.359/pdf.min.js"
+		integrity="sha512-U5C477Z8VvmbYAoV4HDq17tf4wG6HXPC6/KM9+0/wEXQQ13gmKY2Zb0Z2vu0VNUWch4GlJ+Tl/dfoLOH4i2msw==" crossorigin="anonymous"
+		referrerpolicy="no-referrer"></script>
 
-	<title>Document</title>
+	<title>Distribuidora Carmina - Pedidos</title>
 </head>
 <style>
 	.grande{
@@ -24,9 +32,9 @@
 		/*color: #2ecc71;*/
 	}
 	.contenedor{
-		margin-top: 5%;
-		display: flex;
-		gap: 20px;
+		/*margin-top: 5%;*/
+		/*display: flex;*/
+		/*gap: 20px;*/
 	}
 	.left_arrow{
 		width: 15%;
@@ -57,7 +65,9 @@
 		left: 15%;
 		position: relative;
 	}
-
+	.font_black{
+		/*color: black !important;*/
+	}
 </style>
 
 <body>
@@ -89,22 +99,48 @@
 	</ul>
 </div>
 
-<div class="row" style="margin-top: 5%">
+<!-- <div class="row" style="margin-bottom: 0px;">
 	<div class="input-field col s10 offset-s1">
 	    <select>
-	      <option value="" disabled selected>Selecciona el catálogo</option>
+	      <option value="" disabled selected >Selecciona el catálogo</option>
 	      <option value="1">Catálogo 55</option>
 	      <option value="2">Catálogo 58</option>
 	      <option value="3">Catálogo 60</option>
 	    </select>
-	    <label>Selecciona el catálogo</label>
+	   <label>Selecciona el catálogo</label>
 	</div>
-</div>
+</div> -->
 
 <div class="contenedor">
-	<div class="left_arrow"><a href="#"><i class="large material-icons">chevron_left</i></a></div>
+<!-- 	<div class="left_arrow"><a href="#"><i class="large material-icons">chevron_left</i></a></div>
 	<div class="catalogo"><img width="100%" src="images/catalogo.png" alt="catalogo..."></div>
-	<div class="right_arrow"><a href="#"><i class="large material-icons">chevron_right</i></a></div>
+	<div class="right_arrow"><a href="#"><i class="large material-icons">chevron_right</i></a></div> -->
+	<div id="princ">
+		<h3 id="open_h3">Open a PDF file</h3>
+		<canvas style="max-width: 100%;" class="pdf-viewer hidden">
+
+		</canvas>
+	</div>
+	<div id="foot">
+		<ul style="margin-top: 0px;margin-bottom: 0px;">
+			<li>
+				<button id="openPDF">
+					<span>Open</span> <i class="fas fa-folder-open"></i>
+				</button>
+				<input type="file" id="inputFile" hidden>
+			</li>
+			<li class="pagination">
+				<button id="previous"><i class="fas fa-arrow-alt-circle-left"></i></button>
+				<span id="current_page">0 of 0</span>
+				<button id="next"><i class="fas fa-arrow-alt-circle-right"></i></button>
+			</li>
+
+			<li hidden>
+				<span id="zoomValue">150%</span>
+				<input type="range" id="zoom" name="cowbell" min="100" max="300" value="150" step="50" disabled>
+			</li>
+		</ul>
+	</div>
 </div>
 
 <div class="container">
@@ -124,12 +160,15 @@
 </div>
 
 <div class="container center">
-	<a class="waves-effect waves-light btn-large shop red lighten-1" ><i class="material-icons right">shopping_cart</i>Agregar al carrito</a>
+	<a class="waves-effect waves-light btn shop red lighten-1" ><i class="material-icons right">shopping_cart</i>Agregar al carrito</a>
 </div>
+
+
 
 </body>
 </html>
 
+<script type="text/javascript" src="js/viewpdf.js"></script>
 <script>
 	$(document).ready(function(){
     	$('.sidenav').sidenav();
